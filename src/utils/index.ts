e0bdb442588312e2eb32217da7da3a51bc397f1f
El Hassane Boushib <boushib01@gmail.com>
2022-09-20 16:35:29 +0100

    Create game logic

---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+import { ALPHABET } from '../constants'
+
+export const formatToNDigits = (n: number, digits: number) => {
+  return n.toString().padStart(digits, '0')
+}
+
+export const getRandomChar = () =>
+  ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length))
