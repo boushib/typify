0ed226de0011e1c594a370fd0024fb48957baa6a
El Hassane Boushib <boushib.elhassane@gmail.com>
2022-09-20 16:35:29 +0100

    Create game logic

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,16 +1,74 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { GAME_DURATION } from '../../constants'
+import { formatToNDigits, getRandomChar } from '../../utils'
 import './Game.sass'
 
 const Game = () => {
-  const [char, setChar] = useState('S')
+  const [score, setScore] = useState(0)
+  const [char, setChar] = useState(getRandomChar())
+  const [milliseconds, setMilliseconds] = useState(0)
+  const [seconds, setSeconds] = useState(GAME_DURATION)
+  const timer = useRef<ReturnType<typeof setInterval>>()
+  const [isGameOver, setIsGameOver] = useState(false)
+
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key === char) {
+        setScore(score + 1)
+        setChar(getRandomChar())
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [char]
+  )
+
+  useEffect(() => {
+    const date = new Date()
+    timer.current = setInterval(() => handleUpdateTime(date), 1)
+
+    return () => {
+      clearInterval(timer.current)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [handleKeyDown])
+
+  const handleUpdateTime = (startTime: Date) => {
+    const timeDiff = new Date().getTime() - startTime.getTime()
+    const s = GAME_DURATION - Math.floor(timeDiff / 1000) - 1
+    const ms = 1000 - (timeDiff % 1000)
+    setSeconds(s)
+    setMilliseconds(ms)
+
+    if (s <= 0) {
+      setSeconds(0)
+      setMilliseconds(0)
+      setIsGameOver(true)
+      clearInterval(timer.current)
+    }
+  }
+
   return (
     <div className="game page">
       <div className="container">
         <div className="game__header">
-          <div className="game__score">Score: 0</div>
-          <div className="game__time">Time: 06:00</div>
+          <div className="game__score">Score {score}</div>
+          <div className="game__time">
+            <span className="game__time__label">Time </span>
+            <span className="game__time__value">
+              {formatToNDigits(seconds, 2)}:{formatToNDigits(milliseconds, 3)}
+            </span>
+          </div>
         </div>
-        <div className="game__char">{char}</div>
+        {isGameOver && <div className="game__end">Game Over!</div>}
+        {!isGameOver && <div className="game__char">{char}</div>}
       </div>
     </div>
   )
