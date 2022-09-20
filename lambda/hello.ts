915848ecf887217229bf802dd92ab076bcae9ae5
El Hassane Boushib <boushib01@gmail.com>
2022-09-20 21:45:16 +0100

    Current lambda function does not need a cb as it is async

--- lambda/hello.ts
+++ lambda/hello.ts
@@ -1,6 +1,6 @@
 import { Handler } from '@netlify/functions'
 
-export const handler: Handler = async (event, context, cb) => {
+export const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello!' }),
