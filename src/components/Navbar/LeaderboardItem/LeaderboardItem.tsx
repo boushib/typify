3ac1f6f6fbd66b97261e11e18ff639049694777e
El Hassane Boushib <boushib.elhassane@gmail.com>
2022-09-21 02:21:43 +0100

    Integrate getScores lambda into the frontend

---
+++ src/components/Navbar/LeaderboardItem/LeaderboardItem.tsx
@@ -0,0 +1,19 @@
+import './LeaderboardItem.sass'
+
+interface Props {
+  rank: number
+  username: string
+  score: number
+}
+
+const LeaderboardItem = ({ rank, username, score }: Props) => (
+  <div className="leaderboard-item">
+    <div className={`leaderboard-item__rank leaderboard-item__rank--${rank}`}>
+      <span>{rank}</span>
+    </div>
+    <div className="leaderboard-item__username">{username}</div>
+    <div className="leaderboard-item__score">{score}</div>
+  </div>
+)
+
+export default LeaderboardItem
--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -1,9 +1,42 @@
-const Leaderboard = () => (
-  <div className="leaderboard page">
-    <div className="container">
-      <h1>Leaderboard</h1>
+import { useEffect, useState } from 'react'
+import LeaderboardItem from '../../components/Navbar/LeaderboardItem'
+import { ScoreRecord } from '../../models'
+import './Leaderboard.sass'
+
+const Leaderboard = () => {
+  const [scoreRecords, setScoreRecords] = useState<Array<ScoreRecord>>([])
+
+  useEffect(() => {
+    const handleFetchScores = async () => {
+      try {
+        const res = await fetch(`/.netlify/functions/getScores`)
+        const data = (await res.json()) as Array<ScoreRecord>
+        setScoreRecords(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    handleFetchScores()
+  }, [])
+
+  return (
+    <div className="leaderboard page">
+      <div className="container">
+        <h1>Leaderboard</h1>
+        <div className="score-records">
+          {scoreRecords.map(({ id, username, score }, idx) => (
+            <LeaderboardItem
+              rank={idx + 1}
+              username={username}
+              score={score}
+              key={id}
+            />
+          ))}
+        </div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Leaderboard
